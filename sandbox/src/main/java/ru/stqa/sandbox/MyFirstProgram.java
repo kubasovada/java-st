f242794c98dc715b3530ae0d47607a9f57670a4f
Darya <kubasovada@mail.ru>
2021-07-11 13:20:14 +0300

    Вариант 1. Расстояние между двумя точками (без конструктора)

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -3,20 +3,24 @@ package ru.stqa.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        hello("world");
-        hello("user");
-        hello("Darya");
 
-        Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        Point p1 = new Point();
+        p1.x = 6;
+        p1.y = 2;
+        Point p2 = new Point();
+        p2.x = 9;
+        p2.y = 6;
+        System.out.println("Расстояние между думя точками с координатами " + "("+p1.x+";"+ p1.y +")"+ " и " + "("+ p2.x + ";" + p2.y + ")" + " = " + distance(p1, p2));
 
-        Rectangle r = new Rectangle(4,6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+//        Square s = new Square(5);
+//        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+//
+//        Rectangle r = new Rectangle(4,6);
+//        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
+    public static double distance (Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x-p1.x), 2) + Math.pow((p2.y-p1.y), 2));
     }
-    
 
 }
