bc24cac30dc4917dc65f25587bce353e9f18b1f2
Darya <kubasovada@mail.ru>
2021-08-01 15:47:45 +0300

    4.5 Сравнение не количества групп, а длины списка групп

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -4,15 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    //int before = app.getGroupHelper().getGroupCount();
+    // Переменная before содержала количество эл-в, теперь будет содержать список объектов типа GrD
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before + 1);
+    //int after = app.getGroupHelper().getGroupCount();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
 
   }
 }
