238354ca72057a115f7e541a6d1e64fd6233f0b1
Кубасова Дарья Александровна <DKubasova@stream.local>
2021-08-03 17:46:06 +0300

    4.8 Доработан GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -14,10 +15,23 @@ public class GroupCreationTests extends TestBase {
     //int before = app.getGroupHelper().getGroupCount();
     // Переменная before содержала количество эл-в, теперь будет содержать список объектов типа GrD
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData("test1", "test2", "test3");
+    app.getGroupHelper().createGroup(group);
     //int after = app.getGroupHelper().getGroupCount();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    int max = 0;
+    for (GroupData g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
 
   }
 }
