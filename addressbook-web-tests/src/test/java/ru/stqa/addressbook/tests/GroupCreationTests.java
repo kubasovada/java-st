7c6395933246c2a0c3a1bfaec13d7682111366a1
Darya <kubasovada@mail.ru>
2021-08-04 00:02:20 +0300

    4.9 Доработка GroupCreationTests с лямбда-выражением

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -1,7 +1,7 @@
 package ru.stqa.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 
 import java.util.HashSet;
@@ -15,20 +15,14 @@ public class GroupCreationTests extends TestBase {
     //int before = app.getGroupHelper().getGroupCount();
     // Переменная before содержала количество эл-в, теперь будет содержать список объектов типа GrD
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", "test2", "test3");
+    GroupData group = new GroupData("test1111", "test2", "test3");
     app.getGroupHelper().createGroup(group);
     //int after = app.getGroupHelper().getGroupCount();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max( (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
