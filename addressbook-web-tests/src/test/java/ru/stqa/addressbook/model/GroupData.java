a8eedeed235a580728c1e784659c449b9e214ffd
Кубасова Дарья Александровна <DKubasova@stream.local>
2021-08-04 00:27:45 +0300

    4.10 Доработаны GroupCreationTests и GroupModificationTests (Сортировка по Id)

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
@@ -24,8 +24,21 @@ public class GroupData {
     this.id = id;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -35,19 +48,6 @@ public class GroupData {
     this.name = name;
     this.header = header;
     this.footer = footer;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id && Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
   }
 
   public String getName() {
