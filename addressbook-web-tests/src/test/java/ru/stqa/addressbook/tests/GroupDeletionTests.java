ca1ffc8ba4e26ab90fa15397b511aa3330413c1f
Кубасова Дарья Александровна <DKubasova@stream.local>
2021-08-08 17:23:58 +0300

    5.2 Улучшение GroupDeletionTests и GroupModificationTests за счет метода проверки предусловия

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
@@ -2,6 +2,7 @@
 package ru.stqa.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 
@@ -9,20 +10,22 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() throws Exception {
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.getNavigationHelper().gotoGroupPage();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
+  }
+
+  @Test
+  public void testGroupDeletion() throws Exception {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
-
-
     before.remove(before.size() - 1);
     Assert.assertEquals(before, after);
 
