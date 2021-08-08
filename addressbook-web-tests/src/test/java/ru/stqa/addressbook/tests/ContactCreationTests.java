6f93204ad4693377268518b8e4b1da6a1e9a8c33
Кубасова Дарья Александровна <DKubasova@stream.local>
2021-08-08 12:22:40 +0300

    4.9 Доработан ContactCreation с лямбда

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.addressbook.tests;
 
-import org.checkerframework.checker.units.qual.A;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.ContactData;
@@ -14,19 +13,13 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().goToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("test1", "test2", "79999121000", "email@amail.com", "test1");
+    ContactData contact = new ContactData("Lyambda", "test2", "79999121000", "email@amail.com", "test1");
     app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size() + 1 );
 
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
-    int max = 0;
-    for (ContactData g : after) {
-      if (g.getId() > max ) {
-        max = g.getId();
-      }
-    }
-    contact.setId(max);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
