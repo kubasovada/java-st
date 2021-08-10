d92cf2695eb09513ae0239a4189aa7920ddb5868
Кубасова Дарья Александровна <DKubasova@stream.local>
2021-08-11 01:44:57 +0300

    5.6 Hamcrest для групп

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,19 @@
 package ru.stqa.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
+import ru.stqa.addressbook.model.Groups;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTests extends TestBase {
 
@@ -19,16 +27,15 @@ public class GroupModificationTests extends TestBase {
 
   @Test
   public  void testGroupModification () {
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
             .withId(modifiedGroup.getId()).withName("testM").withHeader("test2").withFooter("test3");
     app.group().modify(group);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(before.size(), after.size() );
-    before.remove(modifiedGroup);
-    before.add(group);
-    Assert.assertEquals(before, after);
+    Groups after = app.group().all();
+    assertEquals(before.size(), after.size() );
+
+    assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
 
