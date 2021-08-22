7e8d4823c1265bee3fface3f977cfbbcdea44dcc
Darya <kubasovada@mail.ru>
2021-08-22 16:43:36 +0300

    6.3 Генератор для контактов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,60 @@
+package ru.stqa.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.addressbook.model.ContactData;
+import ru.stqa.addressbook.model.Contacts;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  @Parameter (names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact: contacts) {
+      writer.write(String.format("%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
+              contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(), contact.getEmail()));
+    }
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withFirstname(String.format("Firstname %s", i)).withLastname(String.format("Lastname %s", i))
+              .withHomePhone(String.format("111-%s", i)).withMobilePhone(String.format("222-%s", i)).withWorkPhone("333")
+              .withEmail(String.format("email%s@email.ru", i)));
+    }
+    return contacts;
+  }
+}
