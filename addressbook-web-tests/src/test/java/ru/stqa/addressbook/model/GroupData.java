db672450d12966798b3eee1441099656d847d947
Darya <kubasovada@mail.ru>
2021-08-24 23:33:12 +0300

    7.4 Сравнение данных (модификация, удаление групп)

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
@@ -31,6 +31,19 @@ public class GroupData {
   @Type(type = "text")
   private String footer;
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id && Objects.equals(name, groupData.name) && Objects.equals(header, groupData.header) && Objects.equals(footer, groupData.footer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, header, footer);
+  }
+
   public GroupData withName(String name) {
     this.name = name;
     return this;
@@ -49,19 +62,6 @@ public class GroupData {
   public GroupData withId(int id) {
     this.id = id;
     return this;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id && Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
   }
 
   @Override
