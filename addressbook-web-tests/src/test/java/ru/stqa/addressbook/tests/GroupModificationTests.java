b597e66178d4d5a450eecfb24da4077a178c5157
Darya <kubasovada@mail.ru>
2021-08-25 17:15:21 +0300

    7.5 Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
@@ -27,14 +27,19 @@ public class GroupModificationTests extends TestBase {
 
   @Test
   public  void testGroupModification () {
-    Groups before = app.db().groups();
-    GroupData modifiedGroup = before.iterator().next();
-    GroupData group = new GroupData()
-            .withId(modifiedGroup.getId()).withName("testM").withHeader("test2").withFooter("test3");
-    app.goTo().groupPage();
-    app.group().modify(group);
-    assertThat(app.group().groupCount(), equalTo( before.size()));
-    Groups after = app.db().groups();
-    assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups before = app.db().groups();
+      GroupData modifiedGroup = before.iterator().next();
+      GroupData group = new GroupData()
+              .withId(modifiedGroup.getId()).withName("testM").withHeader("test2").withFooter("test3");
+      app.goTo().groupPage();
+      app.group().modify(group);
+      assertThat(app.group().groupCount(), equalTo(before.size()));
+      Groups after = app.db().groups();
+      assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
+      verifyGroupListInUi();
+    }
   }
+
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.stqa.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +10,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.addressbook.appmanager.ApplicationManager;
+import ru.stqa.addressbook.model.GroupData;
+import ru.stqa.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -40,7 +48,14 @@ public class TestBase {
   @AfterMethod(alwaysRun = true)
   public void logTestStop(Method m) {
     logger.info("Stop test " + m.getName());
+  }
 
+  public void verifyGroupListInUi() {
+    Groups dbGroups = app.db().groups();
+    Groups uiGroups = app.group().all();
+    assertThat(uiGroups, equalTo(dbGroups.stream()
+            .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+            .collect(Collectors.toSet())));
   }
 
 }
