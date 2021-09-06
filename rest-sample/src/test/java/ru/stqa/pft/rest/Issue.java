b2ef41bc53f30e707fd207ad0677ee36d48a4d47
Darya <kubasovada@mail.ru>
2021-09-07 00:59:39 +0300

    RestTests getIssueStatus state_name

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
@@ -0,0 +1,59 @@
+package ru.stqa.pft.rest;
+
+import java.util.Objects;
+
+public class Issue {
+  private int id;
+
+  @Override
+  public String toString() {
+    return "Issue{" +
+            "id=" + id +
+            ", subject='" + subject + '\'' +
+            ", description='" + description + '\'' +
+            '}';
+  }
+
+  private String subject;
+  private String description;
+
+  public int getId() {
+    return id;
+  }
+
+  public Issue withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public Issue withSubject(String subject) {
+    this.subject = subject;
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Issue issue = (Issue) o;
+    return id == issue.id && Objects.equals(subject, issue.subject) && Objects.equals(description, issue.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, subject, description);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Issue withDescription(String description) {
+    this.description = description;
+    return this;
+  }
+}
---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -0,0 +1,83 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestTests {
+  @Test(enabled = false)
+  public void testCreateIssue() throws IOException {
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue().withSubject("TestIssue").withDescription("new Test Issue");
+    int issueId =  createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    assertEquals(newIssues, oldIssues);
+  }
+
+  @Test(enabled = true)
+  public  void testStatus() throws IOException {
+    String issStat = getIssueStatus(1280);
+    System.out.println(issStat);
+  }
+
+  private Set<Issue> getIssues() throws IOException {
+    String json =  getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json")).returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+  }
+
+  private String getIssueStatus(int id) throws IOException {
+    String issueById = getExecutor().execute(Request.Get(String.format("https://bugify.stqa.ru/api/issues/%s.json", id))).returnContent().asString(); // {total
+    JsonElement parsed = new JsonParser().parse(issueById); //JsonObject {total
+    JsonElement issues = parsed.getAsJsonObject().get("issues"); //JsonArray [{assignee
+    JsonElement state_name = issues.getAsJsonObject().get("state_name");
+    System.out.println(state_name);
+
+
+    return null;
+  }
+
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {
+    String json =  getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
+            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                      new BasicNameValuePair("description", newIssue.getDescription())))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+  public boolean isIssueOpen(int issueId) throws IOException {
+    String issueStatus = getIssueStatus(1280);
+    boolean isOpen;
+    if (issueStatus.equals("closed")) {
+      isOpen = false;
+    } else isOpen = true;
+    return isOpen;
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+}
